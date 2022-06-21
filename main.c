a43d2073a13b34bf4b9489a1160435a03898ec35
computermouth <computermouth@crunchbangplusplus.org>
2022-06-20 22:36:59 -0700

    Ghosts in one direction, albiet buggy

--- main.c
+++ main.c
@@ -63,7 +63,7 @@ void draw_destroying(){
 void push_to_destroying(cube_t d){
 	des_count++;
 	destroying = MemRealloc(destroying, sizeof(d) * des_count);
-	d.color = RAYWHITE;
+	//~ d.color = RAYWHITE;
 	destroying[des_count - 1] = d;
 }
 
@@ -83,31 +83,8 @@ cube_t chop_cubes(cube_t top, cube_t last) {
 		.width  = last.width,
 		.height = last.length
 	};
-
-	//~ printf("rtop -- x: %f\ty: %f\tw: %f\th: %f\n", rtop.x, rtop.y, rtop.width, rtop.height);
-	//~ printf("rlas -- x: %f\ty: %f\tw: %f\th: %f\n", rlast.x, rlast.y, rlast.width, rtop.height);
-	
-	//~ float diffx = rtop.x - rlast.x;
-	//~ float diffy = rtop.y - rlast.y;
-	//~ if (rtop.x < rlast.x)
-		//~ diffx = rlast.x - rtop.x;
-	//~ if (rtop.y < rlast.y)
-		//~ diffy = rlast.y - rtop.y;
-	
-	//~ printf("diffx: %f\n", diffx);
-	//~ printf("diffy: %f\n", diffy);
-	
-	//~ if (diffx < 0.05f && diffy < 0.05f) {
-		//~ printf("perfect!\n");
-		//~ // pretend they nailed it to the pixel
-		//~ top.position.x = last.position.x;
-		//~ top.position.z = last.position.z;
-		//~ return top;
-	//~ }
 	
 	Rectangle new     = GetCollisionRec(rtop, rlast);
-	//~ new.x *= diffx;
-	//~ new.y *= diffy;
 	
 	//~ printf("rtop -- x: %f\ty: %f\tw: %f\th: %f\n", rtop.x, rtop.y, rtop.width, rtop.height);
 	//~ printf(" new -- x: %f\ty: %f\tw: %f\th: %f\n", new.x, new.y, new.width, new.height);
@@ -132,36 +109,8 @@ cube_t chop_cubes(cube_t top, cube_t last) {
 		//~ .color = top.color,
 	//~ };
 	
-	cube_t newcube = {
-		.position = {
-			.x = (top.position.x + last.position.x)/2,
-			.y = top.position.y,
-			.z = (top.position.x + last.position.x)/2,
-		},
-		//~ .position = { new.x - (top.width - new.width)/2, top.position.y, new.y - (top.height - new.height)/2},
-		.width = new.width,
-		.height = 0.5f,
-		.length = new.height,
-		.color = top.color,
-	};
-	
-	//~ Vector2 parent_position = { top.position.x / top.width, top.position.z / top.length};
-	//~ newcube.position.x = parent_position.x * newcube.width;
-	//~ newcube.position.z = parent_position.y * newcube.length;
-	//~ float xratio = parent_position.x
-	
 	//~ push_to_destroying(descube);
 	
-	//~ printf("top    width: %f\n", top.width );
-	//~ printf("top    lengt: %f\n", top.length);
-	
-	//~ printf("newdes width: %f\n", descube.width + newcube.width);
-	//~ printf("newdes lengt: %f\n", descube.length + newcube.length);
-	
-	//~ printf("top position: x: %f y:%f z:%f\n", top.position.x, top.position.y, top.position.z);
-	//~ printf("new position: x: %f y:%f z:%f\n", newcube.position.x, newcube.position.y, newcube.position.z);
-	
-	//~ Rectangle new = 
 	return newcube;
 }
 */
@@ -169,51 +118,130 @@ cube_t chop_cubes(cube_t top, cube_t last) {
 cube_t chop_cubes(cube_t curr, cube_t base) {
 	
 	Rectangle curr_rect = {
-		.x      = curr.position.x,
-		.y      = curr.position.z,
+		.x      = curr.position.x - curr.width  / 2,
+		.y      = curr.position.z - curr.length / 2,
 		.width  = curr.width,
 		.height = curr.length
 	};
 	
 	Rectangle base_rect = {
-		.x      = base.position.x,
-		.y      = base.position.z,
+		.x      = base.position.x - base.width  / 2,
+		.y      = base.position.z - base.length / 2,
 		.width  = base.width,
 		.height = base.length
 	};
 	
 	// check for quick return on perfect
-	{
-		float diffx = base_rect.x - curr_rect.x;
-		float diffy = base_rect.y - curr_rect.y;
-		if (base_rect.x < curr_rect.x)
-			diffx = curr_rect.x - base_rect.x;
-		if (base_rect.y < curr_rect.y)
-			diffy = curr_rect.y - base_rect.y;
-		
-		if (diffx < 0.05f && diffy < 0.05f) {
-			printf("perfect!\n");
-			// pretend they nailed it to the pixel
-			curr.position.x = base.position.x;
-			curr.position.z = base.position.z;
-			return curr;
-		}
+	float diffx = base_rect.x - curr_rect.x;
+	float diffy = base_rect.y - curr_rect.y;
+	if (base_rect.x < curr_rect.x)
+		diffx = curr_rect.x - base_rect.x;
+	if (base_rect.y < curr_rect.y)
+		diffy = curr_rect.y - base_rect.y;
+	
+	if (diffx < 0.05f && diffy < 0.05f) {
+		printf("perfect!\n");
+		// pretend they nailed it to the pixel
+		curr.position.x = base.position.x;
+		curr.position.z = base.position.z;
+		return curr;
 	}
 	
-	Rectangle new = GetCollisionRec(curr_rect, base_rect);
+	Rectangle newr = GetCollisionRec(curr_rect, base_rect);
 
-	cube_t newcube = {
+	cube_t newc = {
 		.position = { 
 			.x = (base.position.x + curr.position.x) / 2,
 			.y = curr.position.y,
 			.z = (base.position.z + curr.position.z) / 2,
 		},
-		.width = new.width,
+		.width  = newr.width,
 		.height = 0.5f,
-		.length = new.height,
-		.color = curr.color,
+		.length = newr.height,
+		.color  = curr.color,
 	};
-	return newcube;
+	
+	//~ curr.position.x = (curr.position.x + newc.position.x)/2;
+	//~ curr.position.z = (curr.position.z + newc.position.z)/2;
+				//~ DrawCubeWires(curr.position, curr.width, curr.height, curr.length, MAGENTA     );
+	
+	Vector2 n_ltmid = { newr.x,                  newr.y + newr.height / 2};
+	Vector2 n_rtmid = { newr.x + newr.width,     newr.y + newr.height / 2};
+	Vector2 n_upmid = { newr.x + newr.width / 2, newr.y };
+	Vector2 n_dnmid = { newr.x + newr.width / 2, newr.y + newr.height};
+	
+	//~ Vector2 n_ltmid = { newr.x, newr.y };
+	//~ Vector2 n_rtmid = { newr.x + newr.width, newr.y };
+	//~ Vector2 n_upmid = { newr.x + newr.width, newr.y };
+	//~ Vector2 n_dnmid = { newr.x, newr.y };
+	
+	DrawCubeWires((Vector3){.x = n_ltmid.x, .y=curr.position.y, .z=n_ltmid.y}, 0.1f, 0.1f, 0.1f, GREEN );
+	DrawCubeWires((Vector3){.x = n_rtmid.x, .y=curr.position.y, .z=n_rtmid.y}, 0.1f, 0.1f, 0.1f, GREEN );
+	DrawCubeWires((Vector3){.x = n_upmid.x, .y=curr.position.y, .z=n_upmid.y}, 0.1f, 0.1f, 0.1f, GREEN );
+	DrawCubeWires((Vector3){.x = n_dnmid.x, .y=curr.position.y, .z=n_dnmid.y}, 0.1f, 0.1f, 0.1f, GREEN );
+	
+	Vector2 c_ltmid = { curr_rect.x,                  curr_rect.y + curr_rect.height / 2};
+	Vector2 c_rtmid = { curr_rect.x + curr_rect.width,     curr_rect.y + curr_rect.height / 2};
+	Vector2 c_upmid = { curr_rect.x + curr_rect.width / 2, curr_rect.y };
+	Vector2 c_dnmid = { curr_rect.x + curr_rect.width / 2, curr_rect.y + curr_rect.height};
+	
+	DrawCubeWires((Vector3){.x = c_ltmid.x, .y=curr.position.y, .z=c_ltmid.y}, 0.1f, 0.1f, 0.1f, RED );
+	DrawCubeWires((Vector3){.x = c_rtmid.x, .y=curr.position.y, .z=c_rtmid.y}, 0.1f, 0.1f, 0.1f, RED );
+	DrawCubeWires((Vector3){.x = c_upmid.x, .y=curr.position.y, .z=c_upmid.y}, 0.1f, 0.1f, 0.1f, RED );
+	DrawCubeWires((Vector3){.x = c_dnmid.x, .y=curr.position.y, .z=c_dnmid.y}, 0.1f, 0.1f, 0.1f, RED );
+	
+	Vector2 b_ltmid = { base_rect.x,                       base_rect.y + base_rect.height / 2};
+	Vector2 b_rtmid = { base_rect.x + base_rect.width,     base_rect.y + base_rect.height / 2};
+	Vector2 b_upmid = { base_rect.x + base_rect.width / 2, base_rect.y };
+	Vector2 b_dnmid = { base_rect.x + base_rect.width / 2, base_rect.y + base_rect.height};
+	
+	DrawCubeWires((Vector3){.x = b_ltmid.x, .y=curr.position.y, .z=b_ltmid.y}, 0.1f, 0.1f, 0.1f, MAGENTA );
+	DrawCubeWires((Vector3){.x = b_rtmid.x, .y=curr.position.y, .z=b_rtmid.y}, 0.1f, 0.1f, 0.1f, MAGENTA );
+	DrawCubeWires((Vector3){.x = b_upmid.x, .y=curr.position.y, .z=b_upmid.y}, 0.1f, 0.1f, 0.1f, MAGENTA );
+	DrawCubeWires((Vector3){.x = b_dnmid.x, .y=curr.position.y, .z=b_dnmid.y}, 0.1f, 0.1f, 0.1f, MAGENTA );
+	
+	
+	bool ltcol = CheckCollisionPointRec(b_ltmid, curr_rect);
+	bool rtcol = CheckCollisionPointRec(b_rtmid, curr_rect);
+	bool upcol = CheckCollisionPointRec(b_upmid, curr_rect);
+	bool dncol = CheckCollisionPointRec(b_dnmid, curr_rect);
+	
+	float xpad = 1.0f;
+	float ypad = 1.0f;
+	if(ltcol)
+		xpad = -1.0f;
+	if(upcol)
+		ypad = -1.0f;
+	
+	Rectangle desr;
+	if(upcol){
+		desr.x = b_upmid.x;
+		desr.y = (b_upmid.y + c_upmid.y) / 2;
+		desr.width = curr.width;
+		desr.height = c_upmid.y - b_upmid.y;
+	}
+	
+	printf("lt: %d rt: %d up: %d dn: %d\n", ltcol, rtcol, upcol, dncol);
+	printf("xp: %f yp: %f\n", xpad, ypad);
+	
+	cube_t desc = {
+		.position = {
+			.x = desr.x,
+			.y = curr.position.y,
+			.z = desr.y,
+		},
+		.width  = desr.width,
+		.height = 0.5f,
+		.length = desr.height,
+		.color  = RAYWHITE,
+	};
+	desc.color.a = 64;
+	
+	// TODO -- deleteme
+	//~ if (destroying == NULL)
+		push_to_destroying(desc);
+	
+	return newc;
 }
 
 int main(void)
